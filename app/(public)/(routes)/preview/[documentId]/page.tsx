811ea1d48c19654d6237f78cf46a67765779506a
GrahamQuan <grahamquan24@gmail.com>
2023-10-18 01:48:12 +0800

    Editor

--- app/(public)/(routes)/preview/[documentId]/page.tsx
+++ app/(public)/(routes)/preview/[documentId]/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import { useMemo, type FC } from 'react'
 import { useMutation, useQuery } from 'convex/react'
 import dynamic from 'next/dynamic'
+import { useMemo } from 'react'
 
 import { api } from '@/convex/_generated/api'
 import { Id } from '@/convex/_generated/dataModel'
@@ -10,22 +10,23 @@ import { Toolbar } from '@/components/toolbar'
 import { Cover } from '@/components/cover'
 import { Skeleton } from '@/components/ui/skeleton'
 
-type Props = {
+interface Props {
   params: {
     documentId: Id<'documents'>
   }
 }
 
-const DocumentIdPage: FC<Props> = ({ params }) => {
+const PreviewIdPage = ({ params }: Props) => {
   const Editor = useMemo(
     () => dynamic(() => import('@/components/editor'), { ssr: false }),
     []
   )
-  const update = useMutation(api.documents.update)
 
   const document = useQuery(api.documents.getById, {
     documentId: params.documentId,
   })
+
+  const update = useMutation(api.documents.update)
 
   const onChange = (content: string) => {
     update({
@@ -56,13 +57,17 @@ const DocumentIdPage: FC<Props> = ({ params }) => {
 
   return (
     <div className="pb-40">
-      <Cover url={document.coverImage} />
+      <Cover preview url={document.coverImage} />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
-        <Toolbar initialData={document} />
-        <Editor onChange={onChange} initialContent={document.content} />
+        <Toolbar preview initialData={document} />
+        <Editor
+          editable={false}
+          onChange={onChange}
+          initialContent={document.content}
+        />
       </div>
     </div>
   )
 }
 
-export default DocumentIdPage
+export default PreviewIdPage
